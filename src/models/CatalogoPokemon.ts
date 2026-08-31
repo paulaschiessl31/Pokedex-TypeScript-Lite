90b14067c9addb28cc06f12d78a8db5736d03e42
Paula Schiessl <ana.paulaschiessl31@gmail.com>
2026-08-30 22:45:54 -0300

    feat: adiciona listagem do catalogo

--- src/models/CatalogoPokemon.ts
+++ src/models/CatalogoPokemon.ts
@@ -8,16 +8,30 @@ export class CatalogoPokemon {
 
     // RF08 - Adicionar Pokémon ao catálogo.
     // Verifica se o Pokémon já existe pelo ID antes de adicionar.
-    adicionar(pokemon: PokemonResumo): void {
-
-        const jaExiste = this.pokemons.some((item) => item.id === pokemon.id);
+    adicionar(pokemon: PokemonResumo): void { 
+        const jaExiste = this.pokemons.some((item) => item.id === pokemon.id); 
 
         if (jaExiste) {
-            console.log(`[AVISO] ${pokemon.nome} já está no catálogo.`);
-            return;
-        }
+            console.log(`[AVISO] ${pokemon.nome} já está no catálogo.`); 
+            return; 
+        } 
 
         this.pokemons.push(pokemon);
-        console.log(`[OK] ${pokemon.nome} adicionado ao catálogo.`);
-    }
+        console.log(`[OK] ${pokemon.nome} adicionado ao catálogo.`); 
+    } 
+
+    // RF09 - Listar Pokémon do catálogo.
+    // Verifica se está vazio e usa forEach para mostrar os dados.
+    listar(): void { 
+        if (this.pokemons.length === 0) {
+            console.log("[AVISO] Catálogo vazio."); 
+            return; 
+        } 
+
+        this.pokemons.forEach((pokemon) => {
+            console.log(
+                `#${pokemon.id} - ${pokemon.nome} | Tipos: ${pokemon.tipos.join(",")} | Altura: ${pokemon.altura} | Peso: ${pokemon.peso}`
+            ); 
+        }); 
+    }  
 }
